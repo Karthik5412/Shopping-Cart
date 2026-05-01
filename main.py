09f44cfc2069ab0514298fef1b3fae6e06abdd29
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-05-01 17:13:21 +0530

    going on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from functions import preprocessing, cluster_prediction, items
+from fastapi import FastAPI, Query
+from functions import home_page, items, search_result
+from typing import Annotated, Optional
 
 app = FastAPI()
 
@@ -7,7 +8,20 @@ app = FastAPI()
 def homepage():
     return 'all good'
 
-@app.get('/product/{name}')
-async def return_item(name : str, text : str) :
-    out = await items(name , text)
+@app.get('/all_products/')
+async def main_page():
+    data =  await home_page()
+    return  data
+
+@app.get('/product/{cluster}')
+async def return_item(cluster : int) :
+    out = await items(cluster)
     return out
+
+@app.get('/product_search/')
+async def searching(text : Annotated[str, Query(max_length=10, min_length=3)]) :
+    
+    out = await search_result(text) 
+    
+    return out
+
